15e571bdc305bb1faa3c5cf351091384a3106905
AndreMoura <andre.moura@kinghost.com.br>
2021-01-05 19:00:17 -0300

    Adicionado estilos globais

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -4,14 +4,10 @@ import styled from "styled-components";
 export const HeaderContainer = styled.header`
   width: 100%;
   height: 100%;
-  padding: 30px;
-  padding: 10px 0;
-  top: 0;
+  padding: 1rem 0;
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 12;
-  border-bottom: 2px solid #100f13;
   background: #DB0915;
   position:relative;
     button {
