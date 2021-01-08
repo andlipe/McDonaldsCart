c8d442cc01445fdf5b14f30d2c9e3ae2d2d6ca3e
AndreMoura <andre.moura@kinghost.com.br>
2021-01-08 08:15:45 -0300

    Ajuste na fonte

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap');
+
     * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     outline: none;
+    font-family: Roboto, sans-serif;
     }
     div {
         display: flex;
