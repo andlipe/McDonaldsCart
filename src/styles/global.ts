32553300ffa443bde762a258934e2403900a91d8
AndreMoura <andre.moura@kinghost.com.br>
2021-01-08 10:52:30 -0300

    Adicionado fontes no _document

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap');
 
     * {
     margin: 0;
