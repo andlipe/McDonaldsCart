a58b1ed87a2c03b7267cd6f4960d02b4c1f1123c
AndreMoura <andre.moura@kinghost.com.br>
2021-01-09 19:29:37 -0300

    Adicionado mais produtos

--- src/static-data/static-data-producs.ts
+++ src/static-data/static-data-producs.ts
@@ -37,9 +37,9 @@ let products = [
     {
         id: 5,
         idCategory: 0,
-        name: "Combo",
-        price: 12.0,
-        image: "/assets/MCDonalds.png"
+        name: "McFritas + Bebida",
+        price: 10.9,
+        image: "/assets/McFritasBebida.png"
     },
     {
         id: 6,
@@ -68,7 +68,77 @@ let products = [
         name: "MC Colosso",
         price: 12.0,
         image: "/assets/McColosso.png"
-    }
+    },
+    {
+        id: 10,
+        idCategory: 0,
+        name: "4 Por 19",
+        price: 19,
+        image: "/assets/Combo4.png"
+    },
+    {
+        id: 11,
+        idCategory: 1,
+        name: "BigTasty",
+        price: 25.9,
+        image: "/assets/BigTasty.png"
+    },
+    {
+        id: 12,
+        idCategory: 2,
+        name: "McChicken",
+        price: 22,
+        image: "/assets/McChicken.png"
+    },
+    {
+        id: 13,
+        idCategory: 3,
+        name: "Cheddar Do Mc",
+        price: 10,
+        image: "/assets/Cheddar.png"
+    },
+    {
+        id: 14,
+        idCategory: 4,
+        name: "Sundae",
+        price: 10,
+        image: "/assets/Sundae.png"
+    },
+    {
+        id: 15,
+        idCategory: 0,
+        name: "3 Sanduiches e 3 McFritas",
+        price: 25,
+        image: "/assets/3Sanduiches.png"
+    },
+    {
+        id: 16,
+        idCategory: 1,
+        name: "Picanha Clubhouse",
+        price: 33.9,
+        image: "/assets/PicanhaClub.png"
+    },
+    {
+        id: 17,
+        idCategory: 1,
+        name: "Duplo Barbecue",
+        price: 25.9,
+        image: "/assets/DuploBarbecue.png"
+    },
+    {
+        id: 18,
+        idCategory: 4,
+        name: "Danoninho",
+        price: 5,
+        image: "/assets/Danoninho.png"
+    },
+    {
+        id: 19,
+        idCategory: 1,
+        name: "Duplo Salada",
+        price: 18.9,
+        image: "/assets/DuploSalada.png"
+    },
 ];
 
 export {products};
