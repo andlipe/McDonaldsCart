5f30da011135f2a524aca5b0117f3091c39b42cf
AndreMoura <andre.moura@kinghost.com.br>
2021-01-21 12:13:43 -0300

    Header no endpoint de produtos

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -3,5 +3,7 @@ import { products } from "../../static-data/static-data-products";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200;
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
   res.json(products)
 }
