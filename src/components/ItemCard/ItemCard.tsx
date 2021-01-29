bdca2a25afeacabc96de998fe0c906747844d2cd
AndreMoura <andre.moura@kinghost.com.br>
2021-01-29 14:03:23 -0300

    Ajuste no state de Skeleton Loading

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -8,11 +8,11 @@ const ItemCard = ({ productData }) => {
     const { addToCart } = React.useContext(CartContext);
     let [imageIsLoad, setImageIsLoad] = React.useState(false);
     React.useEffect(() => {
-        setImageIsLoad(!imageIsLoad);
+        setImageIsLoad(true);
     }, [])
     return (
         <Card>
-            {imageIsLoad ? <Image 
+            {imageIsLoad === true ? <Image 
                     src={productData.image}
                     alt={productData.name}
                     width={150}
@@ -20,10 +20,10 @@ const ItemCard = ({ productData }) => {
                     loading={'lazy'}
                 
                 />: <Skeleton height={150} width={150} />}
-            <h3>{imageIsLoad ? productData.name : <Skeleton width={150} height={20}/>}</h3>
-            <p> {imageIsLoad ? 'R$' + productData.price : <Skeleton width={50} height={20}/>}</p>
+            <h3>{imageIsLoad === true ? productData.name : <Skeleton width={150} height={20}/>}</h3>
+            <p> {imageIsLoad === true ? 'R$' + productData.price : <Skeleton width={50} height={20}/>}</p>
             <button onClick={() => addToCart(productData)} >
-                {imageIsLoad ?'Adicionar ao carrinho' : <Skeleton height={30} width={125}/>}
+                {imageIsLoad === true ?'Adicionar ao carrinho' : <Skeleton height={30} width={125}/>}
             </button>
             
         </Card>
